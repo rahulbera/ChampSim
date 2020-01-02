5d8b4ee62212235bf192a058e32492c4a672a10c
Rahul Bera <write2bera@gmail.com>
2020-01-03 00:58:19 +0100

    Bugfix in ChampSim tracer

--- tracer/quick_make.sh
+++ tracer/quick_make.sh
@@ -1,1 +1,1 @@
-make PIN_ROOT=/home/grads/c/cienlux/cienlux/task/pin-3.0-76991-gcc-linux obj-intel64/champsim_tracer.so
+make PIN_ROOT=/mnt/panzer/rahbera/softwares/pin-3.11-97998-g7ecce2dac-gcc-linux obj-intel64/champsim_tracer.so
