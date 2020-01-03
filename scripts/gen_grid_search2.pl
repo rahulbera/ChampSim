80fbf4a9b39f53df0880f186350acdef7b0b9659
Rahul Bera <write2bera@gmail.com>
2020-01-03 10:55:15 +0000

    added grid search script for hyperparameter finding

---
+++ scripts/gen_grid_search2.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use warnings;
+
+my @alpha_list = (0.01, 0.02, 0.1, 0.2, 0.5);
+my @gamma_list = (0.5, 0.75);
+my @epsilon_list = (0.01, 0.1, 0.2, 0.5);
+
+for $alpha (@alpha_list)
+{
+	for $gamma (@gamma_list)
+	{
+		for $epsilon (@epsilon_list)
+		{
+			print "scooby_gs_a_${alpha}_g_${gamma}_e_${epsilon}  \$(BASE) \$(SCOOBY) --scooby_alpha=${alpha} --scooby_gamma=${gamma} --scooby_epsilon=${epsilon}\n";
+		}
+	}
+}
