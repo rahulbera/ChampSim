1638433c90d54481b405b06dc2f23771097d168b
Rahul Bera <write2bera@gmail.com>
2020-03-04 04:07:51 +0100

    added some auxiliary script files

--- scripts/gen_grid_search.pl
+++ scripts/gen_grid_search.pl
@@ -2,31 +2,35 @@
 
 use warnings;
 
-$corr_timely_min 	= 8;
-$corr_timely_max 	= 15;
-$corr_untimely_min 	= -1;
-$corr_untimely_max 	= 8;
-$incorr_min 		= -8;
-$incorr_max 		= 2;
-$none_min 		= -14;
-$none_max 		= 0;
-$out_of_bounds_min 	= -14;
-$out_of_bounds_max 	= 0;
+$corr_timely_min 	= 9;
+$corr_timely_max 	= 20;
+$corr_untimely_min 	= 0;
+$corr_untimely_max 	= 15;
+$incorr_min 		= -10;
+$incorr_max 		= 0;
+$none_min 		= -16;
+$none_max 		= -2;
+$out_of_bounds_min 	= -16;
+$out_of_bounds_max 	= -2;
 
+my $total_exps = 0;
 for(my $rew_corr_timely = $corr_timely_min; $rew_corr_timely <= $corr_timely_max; $rew_corr_timely += 2)
 {
 	for(my $rew_corr_untimely = $corr_untimely_min; $rew_corr_untimely <= $corr_untimely_max; $rew_corr_untimely += 2)
 	{
 		for(my $rew_incorr = $incorr_min; $rew_incorr <= $incorr_max; $rew_incorr += 2)
 		{
-			for(my $rew_none = $none_min; $rew_none <= $none_max; $rew_none += 2)
+			for(my $rew_none = $none_min; $rew_none <= $none_max; $rew_none += 3)
 			{
-				for(my $rew_out_of_bounds = $out_of_bounds_min; $rew_out_of_bounds <= $out_of_bounds_max; $rew_out_of_bounds += 2)
+				for(my $rew_out_of_bounds = $out_of_bounds_min; $rew_out_of_bounds <= $out_of_bounds_max; $rew_out_of_bounds += 3)
 				{
-					print "scooby_gs_${rew_corr_timely}_${rew_corr_untimely}_${rew_incorr}_${rew_none}_${rew_out_of_bounds}   \$(BASE) \$(SCOOBY) --scooby_reward_correct_timely=${rew_corr_timely} --scooby_reward_correct_untimely=${rew_corr_untimely} --scooby_reward_incorrect=${rew_incorr} --scooby_reward_none=${rew_none} --scooby_reward_out_of_bounds=${rew_out_of_bounds}\n";
+					print STDOUT "scooby_gs2_${rew_corr_timely}_${rew_corr_untimely}_${rew_incorr}_${rew_none}_${rew_out_of_bounds}   \$(BASE) \$(SCOOBY_CMAC) --scooby_reward_correct_timely=${rew_corr_timely} --scooby_reward_correct_untimely=${rew_corr_untimely} --scooby_reward_incorrect=${rew_incorr} --scooby_reward_none=${rew_none} --scooby_reward_out_of_bounds=${rew_out_of_bounds}\n";
+					$total_exps++;
 				}
 			}
 		}
 	}
-} 
+}
+
+print STDERR "Total experiemtns: $total_exps";
 
