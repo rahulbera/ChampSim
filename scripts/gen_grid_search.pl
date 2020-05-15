919f30265063d240d3a6cb56edfcc0635d775f4b
Rahul Bera <write2bera@gmail.com>
2020-05-15 17:56:29 +0200

    changed grid_search_script

--- scripts/gen_grid_search.pl
+++ scripts/gen_grid_search.pl
@@ -3,14 +3,14 @@
 use warnings;
 
 $corr_timely_min 	= 9;
-$corr_timely_max 	= 20;
-$corr_untimely_min 	= 0;
-$corr_untimely_max 	= 15;
-$incorr_min 		= -10;
-$incorr_max 		= 0;
+$corr_timely_max 	= 24;
+$corr_untimely_min 	= 2;
+$corr_untimely_max 	= 16;
+$incorr_min 		= -12;
+$incorr_max 		= 2;
 $none_min 		= -16;
 $none_max 		= -2;
-$out_of_bounds_min 	= -16;
+$out_of_bounds_min 	= -12;
 $out_of_bounds_max 	= -2;
 
 my $total_exps = 0;
@@ -24,7 +24,7 @@ for(my $rew_corr_timely = $corr_timely_min; $rew_corr_timely <= $corr_timely_max
 			{
 				for(my $rew_out_of_bounds = $out_of_bounds_min; $rew_out_of_bounds <= $out_of_bounds_max; $rew_out_of_bounds += 3)
 				{
-					print STDOUT "scooby_gs2_${rew_corr_timely}_${rew_corr_untimely}_${rew_incorr}_${rew_none}_${rew_out_of_bounds}   \$(BASE) \$(SCOOBY_CMAC) --scooby_reward_correct_timely=${rew_corr_timely} --scooby_reward_correct_untimely=${rew_corr_untimely} --scooby_reward_incorrect=${rew_incorr} --scooby_reward_none=${rew_none} --scooby_reward_out_of_bounds=${rew_out_of_bounds}\n";
+					print STDOUT "scooby_gs15May_${rew_corr_timely}_${rew_corr_untimely}_${rew_incorr}_${rew_none}_${rew_out_of_bounds}   \$(BASE) \$(SCOOBY_CMAC) --scooby_reward_correct_timely=${rew_corr_timely} --scooby_reward_correct_untimely=${rew_corr_untimely} --scooby_reward_incorrect=${rew_incorr} --scooby_reward_none=${rew_none} --scooby_reward_out_of_bounds=${rew_out_of_bounds}\n";
 					$total_exps++;
 				}
 			}
